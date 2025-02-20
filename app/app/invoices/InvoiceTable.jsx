2f36a9f59d3d1b32c84db551a67c755fc3105026
Sharjun Hussain <sharjoonibnuhussain99@gmail.com>
2025-02-20 12:51:44 +0530

    invoice page created

---
+++ app/app/invoices/InvoiceTable.jsx
@@ -0,0 +1,263 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import { Button } from "@/components/ui/button";
+import * as React from "react";
+import { Input } from "@/components/ui/input";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getSortedRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
+
+import { ArrowUpDown, Pencil, Trash2, MoreHorizontal } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import axios from "axios";
+
+// import AddOfficeModal from "../addbranchmodal";
+// import useMediaQuery from "@/Hooks/useMediaQuery";
+
+export function InvoiceTable({ data, width, loading, onUpdate, onDelete }) {
+  const [sorting, setSorting] = React.useState([]);
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [columnFilters, setColumnFilters] = React.useState([]);
+  const [OpenModal, setOpenModal] = React.useState(false);
+  // const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const columns = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+
+    {
+      accessorKey: "invoice_no",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      header: "Invoice No",
+    },
+
+    {
+      accessorKey: "user",
+      header: "User",
+    },
+    {
+      accessorKey: "total_amount",
+      header: "Total Amount",
+    },
+    {
+      accessorKey: "paid",
+      header: "Paid",
+    },
+    {
+      accessorKey: "balance",
+      header: "Balance",
+    },
+    {
+      accessorKey: "status",
+      header: "Status",
+    },
+  ];
+
+  const table = useReactTable({
+    data: data ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className={`${width}`}>
+      <div className="joon-card w-full">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between py-4">
+            <Input
+              placeholder="Filter Users"
+              value={table.getColumn("full_name")?.getFilterValue() ?? ""}
+              onChange={(event) =>
+                table.getColumn("full_name")?.setFilterValue(event.target.value)
+              }
+              className="w-full"
+            />
+          </div>
+          <div>
+            <Button onClick={() => setOpenModal(true)}>Add Users</Button>
+          </div>
+        </div>
+        <div>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow
+                  className="border-b-gray-400 dark:border-b-gray-500 border-opacity-100"
+                  key={headerGroup.id}
+                >
+                  {headerGroup.headers.map((header) => (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  ))}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {loading ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="text-center h-12"
+                  >
+                    Loading...
+                  </TableCell>
+                </TableRow>
+              ) : table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    className="border-white border-opacity-10"
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-12 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex items-center justify-between py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
+          </div>
+          <div className="flex flex-wrap items-center space-x-2">
+            <span className="text-sm">Rows per page: </span>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value));
+              }}
+              className="border rounded-md p-1"
+            >
+              {[5, 10, 20, 50].map((pageSize) => (
+                <option
+                  className="bg-secondary"
+                  key={pageSize}
+                  value={pageSize}
+                >
+                  {pageSize}
+                </option>
+              ))}
+            </select>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {/* {isMobile ? "<<" : "Previous"} */}
+            </Button>
+            <span className="text-xs">
+              Page {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </span>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {/* {isMobile ? ">>" : "Next"} */}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
